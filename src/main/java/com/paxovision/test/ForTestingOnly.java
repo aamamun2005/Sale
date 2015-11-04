942e357b5d98eebbbe87968779b4e37adfc9be09
PaxoTech Student <PaxoTech Student@PaxoTechWin7X64.paxotech.com>
2015-11-04 15:01:45 -0500

    Mamun first

---
+++ src/main/java/com/paxovision/test/ForTestingOnly.java
@@ -0,0 +1,14 @@
+package com.paxovision.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ForTestingOnly {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
